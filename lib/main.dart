72c758137dc611776d56a95b4ffae7a15a013b58
tanya8266 <tanya8266y@gmail.com>
2025-03-10 19:38:20 +0530

    fixed errors

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,6 @@ class MyApp extends StatelessWidget {
           ),
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
-        home: LoginScreen());
+        home: HomeScreen());
   }
 }
