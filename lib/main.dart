b337b86121a7af58e10e956f08b6b347828e6b63
tanya8266 <tanya8266y@gmail.com>
2025-03-12 23:33:43 +0530

    register screen for parent has been made

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:title_proj/LoginScreen.dart';
+import 'package:title_proj/child/LoginScreen.dart';
 import 'package:title_proj/home_screen.dart';
 
 void main() {
